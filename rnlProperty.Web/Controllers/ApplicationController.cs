248add06dab865afe1a0b36f1c347607460b1e50
Lori Peterson <lori.f.peterson@gmail.com>
2018-04-06 15:03:57 -0500

    First Draft of website

---
+++ rnlProperty.Web/Controllers/ApplicationController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace rnlProperty.Web.Controllers
+{
+    public class ApplicationController : Controller
+    {
+        // GET: Application
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Qualifications()
+        {
+            return View();
+        }
+
+    }
+}
